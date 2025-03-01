d7715e0c19714e9b91e230997dfdc6a8c08e0490
aaransmith11 <aaransmith0817@gmail.com>
2025-03-01 11:37:10 -0600

    finished project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+generateDomains()
 };
+const generateDomains = ()=>{
+  let first = ["pen", "soup", "duck", "coffee", "robot"]
+  let second = ["island", "room", "cheese", "lilo", "stitch"]
+  let third = [".com", ".edu", ".net" , ".org", ".uk"]
+
+  for (let i = 0; i < first.length; i++){
+    for (let j = 0; j < second.length; j++){
+      for (let k = 0; k < third.length; k++){
+        console.log(first[i] + second[j] + third[k])
+      }
+    }
+  }
+}
